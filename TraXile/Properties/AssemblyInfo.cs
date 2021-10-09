e7b65b285f41688ff0fa30463c25df047fd3a53f
dermow <s.seibold@posteo.de>
2021-10-09 11:29:33 +0200

    assembly info added

--- TraXile/Properties/AssemblyInfo.cs
+++ TraXile/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("PoE_Tracker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TraXile")]
+[assembly: AssemblyDescription("Tracking Tool for Path of Exile")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("dermow")]
 [assembly: AssemblyProduct("PoE_Tracker")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
 // für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
 // COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("2d010e92-b659-4567-b1d0-9f6474d124e2")]
